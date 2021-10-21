01b6c479b4f7b76d450db764d8723ef12a88fb60
Mohamed Ben <b.mohamed@tomba.io>
2021-10-21 15:21:55 +0100

    v 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name = 'tomba-io',
-  version = '1.0.0',
+  version = '1.0.1',
   license='Apache-2.0',
   description = 'Tomba.io is an Email Finder for B2B sales and email marketing',
   long_description=long_description,
